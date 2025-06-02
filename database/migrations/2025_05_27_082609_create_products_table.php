2d13c97b3a9453831684568f6653d9d3e81cce80
NiBienvenu <nicibienvenu@gmail.com>
2025-06-02 08:14:52 +0200

    create products

--- database/migrations/2025_05_27_082609_create_products_table.php
+++ database/migrations/2025_05_27_082609_create_products_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->foreignId('agency_id')->nullable()->constrained();
             $table->foreignId('created_by')->constrained('users');
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
