5a054dd84caaf85b93316d2af186ce0ed5814f5e
Jean Lionel <nijeanlionel@gmail.com>
2025-06-25 06:40:41 +0200

    display product with code

--- resources/views/livewire/stock/add-product-stock.blade.php
+++ resources/views/livewire/stock/add-product-stock.blade.php
@@ -25,11 +25,24 @@
 
         <div class="card">
             <div class="card-body">
+                <h5 class="card-title">Les produits du stock {{ $stock->name }}</h5>
+                <div class="mb-3 input-group">
+    <input
+        type="text"
+        wire:model.live="stockProductSearch"
+        wire:keyup.debounce.300ms="searchStockProducts"
+        placeholder="Rechercher un produit"
+        class="form-control"
+    >
+    <button class="btn btn-primary" type="button" wire:click="searchStockProducts">
+        <i class="bi bi-search"></i> Rechercher
+    </button>
+</div>
                 <table id="stockProductsTable" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Product ID</th>
+                            <th>CODE</th>
                             <th>Product Name</th>
                             <th>Quantity</th>
                             <th>Date </th>
@@ -40,12 +53,11 @@
                         @foreach($stockProducts as $product)
                         <tr>
                             <td>{{ $product->id }}</td>
-                            <td>{{ $product->product_id }}</td>
+                            <td>{{ $product->product->code }}</td>
                             <td>{{ $product->product->name }}</td>
                             <td>{{ $product->quantity }}</td>
                             <td>{{ \Carbon\Carbon::parse($product->created_at)->format('Y-m-d H:i:s') }}</td>
-                            <td>
-
+                            <td class="text-nowrap">
                                 <a href="{{ route('stocks.mouvement', $product->id) }}" class="btn btn-info btn-sm"> <i class="bi bi-eye"></i> Mouvement</a>
 
                                 <button wire:click="removeProduct({{ $product->id }})" class="btn btn-danger btn-sm"> <i class="bi bi-cart-x"></i> Supprimer</button>
