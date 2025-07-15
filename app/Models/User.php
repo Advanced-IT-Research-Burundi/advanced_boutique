d5a4336ad998c6568ee65d04246f7b2de6280eac
NiBienvenu <nicibienvenu@gmail.com>
2025-07-15 08:26:48 +0200

    appand fullname and name

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,6 +83,7 @@ class User extends Authenticatable
         'name',
         'full_name',
     ];
+
     public function getNameAttribute(): string
     {
         return "{$this->first_name} {$this->last_name}";
